361947bb1cf383fdfefbac4e0746722ad5f7524f
cHe3Ss <92560670+L2342@users.noreply.github.com>
2025-08-29 22:55:42 -0500

    Ajuste de CORS

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Tu frontend
+    origin: true, // Tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
--- web-app/src/pages/Checkout/CheckoutForm.jsx
+++ web-app/src/pages/Checkout/CheckoutForm.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Checkout.css';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const CheckoutForm = ({ cartItems, clearCart }) => {
     const stripe = useStripe();
